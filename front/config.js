ec2c6519ac0f9bb592643aa55d835ab4693e6339
zhf_sy <zhf_sy@163.com>
2025-12-14 15:03:05 +0800

    youhua

--- front/config.js
+++ front/config.js
@@ -2,7 +2,7 @@ window.ganConfig = {
     envs: [
         {
             name: 'Default',
-            value: 'dev',
+            value: 'default',
             url: 'http://127.0.0.1:9527'
         },
         {
@@ -14,6 +14,12 @@ window.ganConfig = {
             name: 'stag',
             value: 'stag',
             url: 'http://192.168.31.44:9528'
+        },
+        {
+            name: 'prod',
+            value: 'prod',
+            url: 'http://192.168.31.44:9527'
         }
-    ]
+    ],
+    categories: ['', 'java', 'node', 'dockerfile']
 }
