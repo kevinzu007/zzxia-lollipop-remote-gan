f19baa551942cbbc7d4488b03bc635b38ee6d584
zhf_sy <zhf_sy@163.com>
2025-12-14 11:18:49 +0800

    add A、附加参数

---
+++ front/config.js
@@ -0,0 +1,19 @@
+window.ganConfig = {
+    envs: [
+        {
+            name: 'Default',
+            value: 'dev',
+            url: 'http://127.0.0.1:9527'
+        },
+        {
+            name: 'dev',
+            value: 'dev',
+            url: 'http://192.168.31.44:9527'
+        },
+        {
+            name: 'stag',
+            value: 'stag',
+            url: 'http://192.168.31.44:9528'
+        }
+    ]
+}
